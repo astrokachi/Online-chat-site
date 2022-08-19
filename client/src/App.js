d960adc9448fe0fd1f288a2f0415a44a0864cd9b
Kachi <astrokachi@gmail.com>
2022-08-19 22:05:57 +0100

    updated app logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Chatter }from './components/Chatter';
 import { Auth } from './components/Auth';
 import Cookies from 'universal-cookie';
 import { StreamChat } from 'stream-chat';
+import 'stream-chat-react/dist/css/index.css'
+import './App.css'
 
 const cookies = new Cookies()
 
@@ -11,23 +13,27 @@ const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
 
+
 if(authToken) {
     client.connectUser({
         id: cookies.get('userId'),
-        name: cookies.get('name'),
+        username: cookies.get('username'),
         hashedPassword: cookies.get('hashedPassword'),
         email: cookies.get('email')
     }, authToken)
 }
 
 function App() {
+  const [createType, setCreateType] = useState();
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
 
   if(!authToken) return <Auth />
 
   else return (
     <div className="mx-auto w-full h-full">
-      <Chatter />
+      <Chatter isCreating={isCreating} setIsCreating={setIsCreating} setIsEditing={setIsEditing} isEditing={isEditing} createType={createType} setCreateType={setCreateType} />
     </div>
   );
 }
