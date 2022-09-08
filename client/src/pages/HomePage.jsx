91806cb53e84a3bf51a9bb2a2dd3204f66619fa3
Kachi <astrokachi@gmail.com>
2022-09-08 01:37:27 +0100

    updates

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -41,7 +41,7 @@ export const HomePage = () => {
       <Nav />
       <div className="flex w-full" onClick={() => setIsToggle(true)}>
         <div className="bg-gradient-to-b w-full pb-24 transition-all ease duration-150 min-h-screen text-white from-start text-center to-black sm:px-6 md:px-14 py-32">
-          <p className="font-medium text-lg text-left pt-8 pb-2 px-3">{pref ? `${pref.toUpperCase()}` : 'MODELS'}</p>
+          <p className="font-medium text-lg text-left pt-8 pb-2 px-3">{pref ? `${pref.toUpperCase()}` : 'ALL MODELS'}</p>
           <div
             id="container"
             className="grid grid-cols-2 md:grid-cols-3  lg:grid-cols-5 gap-x-8 px-3  lg:gap-x-5 gap-y-8 "
